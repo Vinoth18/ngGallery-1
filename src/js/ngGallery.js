a4552cbf672295f1f6f77007eb9f40e6c3ae916d
Vinoth <vkuppusamy@aconex.com>
2016-06-30 17:20:41 +0530

    Customize the plugin according to the application usage
    - Added icons for close and left, right navigations
    - Showing only one thumbnail along with the number
    of photos count
    - Launching the gallery on clicking on the photo
    count
    - Added header and footer informations

--- src/js/ngGallery.js
+++ src/js/ngGallery.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use-strict';
 
     angular.module('jkuri.gallery', []).directive('ngGallery', ngGallery);
@@ -26,54 +26,27 @@
             scope.thumbsNum = scope.thumbsNum || 3; // should be odd
         }
 
-        var template_url = defaults.templateUrl;
-        // Set the default template
-        $templateCache.put(template_url,
-            '<div class="{{ baseClass }}">' +
-            '  <div ng-repeat="i in images">' +
-            '    <img ng-src="{{ i.thumb }}" class="{{ thumbClass }}" ng-click="openGallery($index)" alt="Image {{ $index + 1 }}" />' +
-            '  </div>' +
-            '</div>' +
-            '<div class="ng-overlay" ng-show="opened">' +
-            '</div>' +
-            '<div class="ng-gallery-content" unselectable="on" ng-show="opened" ng-swipe-left="nextImage()" ng-swipe-right="prevImage()">' +
-            '  <div class="uil-ring-css" ng-show="loading"><div></div></div>' +
-            '<a href="{{getImageDownloadSrc()}}" target="_blank" ng-show="showImageDownloadButton()" class="download-image"><i class="fa fa-download"></i></a>' +
-            '  <a class="close-popup" ng-click="closeGallery()"><i class="fa fa-close"></i></a>' +
-            '  <a class="nav-left" ng-click="prevImage()"><i class="fa fa-angle-left"></i></a>' +
-            '  <img ondragstart="return false;" draggable="false" ng-src="{{ img }}" ng-click="nextImage()" ng-show="!loading" class="effect" />' +
-            '  <a class="nav-right" ng-click="nextImage()"><i class="fa fa-angle-right"></i></a>' +
-            '  <span class="info-text">{{ index + 1 }}/{{ images.length }} - {{ description }}</span>' +
-            '  <div class="ng-thumbnails-wrapper">' +
-            '    <div class="ng-thumbnails slide-left">' +
-            '      <div ng-repeat="i in images">' +
-            '        <img ng-src="{{ i.thumb }}" ng-class="{\'active\': index === $index}" ng-click="changeImage($index)" />' +
-            '      </div>' +
-            '    </div>' +
-            '  </div>' +
-            '</div>'
-        );
-
         return {
             restrict: 'EA',
             scope: {
                 images: '=',
                 thumbsNum: '@',
-                hideOverflow: '='
+                hideOverflow: '=',
+                photoLabel: '='
             },
             controller: [
                 '$scope',
-                function ($scope) {
-                    $scope.$on('openGallery', function (e, args) {
+                function($scope) {
+                    $scope.$on('openGallery', function(e, args) {
                         $scope.openGallery(args.index);
                     });
                 }
             ],
-            templateUrl: function (element, attrs) {
-                return attrs.templateUrl || defaults.templateUrl;
-            },
-            link: function (scope, element, attrs) {
+            templateUrl: '../common/resources/vendor/scripts/ng-gallery/js/ngGalleryTemplate.html',
+            link: function(scope, element, attrs) {
                 setScopeValues(scope, attrs);
+
+                scope.image = scope.images[0];
 
                 if (scope.thumbsNum >= 11) {
                     scope.thumbsNum = 11;
@@ -89,11 +62,11 @@
                 scope.thumb_wrapper_width = 0;
                 scope.thumbs_width = 0;
 
-                var loadImage = function (i) {
+                var loadImage = function(i) {
                     var deferred = $q.defer();
                     var image = new Image();
 
-                    image.onload = function () {
+                    image.onload = function() {
                         scope.loading = false;
                         if (typeof this.complete === false || this.naturalWidth === 0) {
                             deferred.reject();
@@ -101,7 +74,7 @@
                         deferred.resolve(image);
                     };
 
-                    image.onerror = function () {
+                    image.onerror = function() {
                         deferred.reject();
                     };
 
@@ -111,31 +84,32 @@
                     return deferred.promise;
                 };
 
-                var showImage = function (i) {
-                    loadImage(scope.index).then(function (resp) {
+                var showImage = function(i) {
+                    loadImage(scope.index).then(function(resp) {
                         scope.img = resp.src;
                         smartScroll(scope.index);
                     });
                     scope.description = scope.images[i].description || '';
+                    scope.currentImage = scope.images[i];
                 };
 
-                scope.showImageDownloadButton = function () {
+                scope.showImageDownloadButton = function() {
                     if (scope.images[scope.index] == null || scope.images[scope.index].downloadSrc == null) return
                     var image = scope.images[scope.index];
                     return angular.isDefined(image.downloadSrc) && 0 < image.downloadSrc.length;
                 };
 
-                scope.getImageDownloadSrc = function () {
+                scope.getImageDownloadSrc = function() {
                     if (scope.images[scope.index] == null || scope.images[scope.index].downloadSrc == null) return
                     return scope.images[scope.index].downloadSrc;
                 };
 
-                scope.changeImage = function (i) {
+                scope.changeImage = function(i) {
                     scope.index = i;
                     showImage(i);
                 };
 
-                scope.nextImage = function () {
+                scope.nextImage = function() {
                     scope.index += 1;
                     if (scope.index === scope.images.length) {
                         scope.index = 0;
@@ -143,7 +117,7 @@
                     showImage(scope.index);
                 };
 
-                scope.prevImage = function () {
+                scope.prevImage = function() {
                     scope.index -= 1;
                     if (scope.index < 0) {
                         scope.index = scope.images.length - 1;
@@ -151,35 +125,43 @@
                     showImage(scope.index);
                 };
 
-                scope.openGallery = function (i) {
+                scope.openGallery = function(i) {
                     if (typeof i !== undefined) {
                         scope.index = i;
                         showImage(scope.index);
                     }
                     scope.opened = true;
                     if (scope.hideOverflow) {
-                        $('body').css({overflow: 'hidden'});
+                        $('body').css({
+                            overflow: 'hidden'
+                        });
                     }
 
-                    $timeout(function () {
+                    $timeout(function() {
                         var calculatedWidth = calculateThumbsWidth();
                         scope.thumbs_width = calculatedWidth.width;
                         //Add 1px, otherwise some browsers move the last image into a new line
                         var thumbnailsWidth = calculatedWidth.width + 1;
-                        $thumbnails.css({width: thumbnailsWidth + 'px'});
-                        $thumbwrapper.css({width: calculatedWidth.visible_width + 'px'});
+                        $thumbnails.css({
+                            width: thumbnailsWidth + 'px'
+                        });
+                        $thumbwrapper.css({
+                            width: calculatedWidth.visible_width + 'px'
+                        });
                         smartScroll(scope.index);
                     });
                 };
 
-                scope.closeGallery = function () {
+                scope.closeGallery = function() {
                     scope.opened = false;
                     if (scope.hideOverflow) {
-                        $('body').css({overflow: ''});
+                        $('body').css({
+                            overflow: ''
+                        });
                     }
                 };
 
-                $body.bind('keydown', function (event) {
+                $body.bind('keydown', function(event) {
                     if (!scope.opened) {
                         return;
                     }
@@ -195,10 +177,10 @@
                     scope.$apply();
                 });
 
-                var calculateThumbsWidth = function () {
+                var calculateThumbsWidth = function() {
                     var width = 0,
                         visible_width = 0;
-                    angular.forEach($thumbnails.find('img'), function (thumb) {
+                    angular.forEach($thumbnails.find('img'), function(thumb) {
                         width += thumb.clientWidth;
                         width += 10; // margin-right
                         visible_width = thumb.clientWidth + 10;
@@ -209,8 +191,8 @@
                     };
                 };
 
-                var smartScroll = function (index) {
-                    $timeout(function () {
+                var smartScroll = function(index) {
+                    $timeout(function() {
                         var len = scope.images.length,
                             width = scope.thumbs_width,
                             item_scroll = parseInt(width / len, 10),
